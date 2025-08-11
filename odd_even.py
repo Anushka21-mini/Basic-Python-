ef9a05aab4f7fac828f60375d243390be849f7a0
Anushka K. <anushkakhara03@gmail.com>
2025-08-11 22:30:25 +0530

    Add files via upload

---
+++ odd_even.py
@@ -0,0 +1,17 @@
+number = 8
+print(number % 2)
+if number % 2 != 0:
+    print("odd")
+else:
+    print("even")
+
+
+def odd_even(integer):
+    if integer % 2 != 0:
+        print("odd")
+    else:
+        print('even')
+
+
+odd_even(7)
+odd_even(8)
